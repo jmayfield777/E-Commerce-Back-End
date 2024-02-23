2c4de61b8cf7ae24812d5adefe1950e0ff078147
Bacala <bacala@bacalas-mbp.mynetworksettings.com>
2024-02-23 12:17:27 -0500

    fixed get route and get id route in tag-route.js

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -6,7 +6,7 @@ const { Tag, Product, ProductTag } = require('../../models');
 router.get('/', async (req, res) => {
   try {
     const tagData = await Tag.findAll({
-      include: [{  model: Product }],
+      include: [{  model: Product, through: ProductTag, as: 'productTag_products' }],
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -17,7 +17,7 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product }],
+      include: [{ model: Product, through: ProductTag, as: 'productTag_products' }],
     });
 
     if (!tagData) {
